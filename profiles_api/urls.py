e3263a46aa42015e1b4b2969cc9cd2e99bd81bac
code2-tech <astha.ramwala@gmail.com>
2020-01-27 17:59:35 +0530

    Added viewset to profile api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+route = DefaultRouter()
+route.register("hello-vs", views.HelloViewset, basename="hello-vs")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello_api/', views.HelloApiView.as_view())
+    path('hello_api', views.HelloApiView.as_view()),
+    path('', include(route.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,60 @@
+from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import HelloSerializer
 
 
 class HelloApiView(APIView):
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         an_api = ['feature1', 'feature2', 'feature3']
         return Response({"message": "Hello", "an_api": an_api})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({"message": "Put"})
+
+    def patch(self, request, pk=None):
+        return Response({"message": "Patch"})
+
+    def delete(self, request, pk=None):
+        return Response({"message": "Delete"})
+
+
+class HelloViewset(viewsets.ViewSet):
+    serializable_class = HelloSerializer
+
+    def list(self, request):
+        a_viewset = ['feature1', 'feature2', 'feature3']
+        return Response({"message": "Hello", "a_viewset": a_viewset})
+
+    def retrieve(self, request, pk=None):
+        return Response({"method": "GET"})
+
+    def create(self, request):
+        serialize = self.serializable_class(data=request.data)
+
+        if serialize.is_valid():
+            return Response({"name": serialize.validated_data["name"]})
+        else:
+            return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, req, pk=None):
+        return Response({"method": "PUT"})
+
+    def partial_update(self, req, pk=None):
+        return Response({"method": "PATCH"})
+
+    def destroy(self, req, pk=None):
+        return Response({"method": "DELETE"})
