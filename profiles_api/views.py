7ba4e81f934381f8577ee259d71cb60f0d2e41b9
code2-tech <astha.ramwala@gmail.com>
2020-01-30 18:50:28 +0530

    Made private feed of user

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.settings import api_settings
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import models, permissions
 from .serializers import HelloSerializer, UserProfileSerializer, DemoSerializer, UserfeedSerializer
@@ -90,7 +90,7 @@ class UserFeedViewset(viewsets.ModelViewSet):
     serializer_class = UserfeedSerializer
     permission_classes = (
         permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly
+        IsAuthenticated
     )
     queryset = models.ProfileFeedItem.objects.all()
 
